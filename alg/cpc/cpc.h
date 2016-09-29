aea45f39323bd210296d173651117c88c1263bbd
cw <845206468@qq.com>
2016-09-29 18:30:48 +0800

    common

--- alg/cpc/cpc.h
+++ alg/cpc/cpc.h
@@ -12,3 +12,28 @@ typedef void (* FUNC_GET_ALG_RESULT)(char* alg_result, int len, void* user_data)
 int cpc_start(FUNC_GET_ALG_RESULT func);
 int cpc_stop();
 int cpc_set_params(void* params, int len);
+
+class CPC_convertor{
+    virtual convert_in(void* out, int len, IMP_CPC_Para_S* para)=0; 
+}
+
+class CPC_Configer{
+    
+    
+}
+
+//windows
+class CPC{
+public:
+    start();
+    stop();
+    config();
+private:
+    
+   
+
+}
+local_cpc_start();
+local_cpc_config();
+param_convert_json(char* json, IMP_CPC_Para_S* result);
+params_convert_json
